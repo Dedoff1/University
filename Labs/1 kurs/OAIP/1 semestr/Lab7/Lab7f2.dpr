2447a6d9a2bee0d45396d2c9a7cffc7bdf71f2c2
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-18 02:18:36 +0300

    Add files via upload

---
+++ Labs/1 kurs/OAIP/1 semestr/Lab7/Lab7f2.dpr
@@ -0,0 +1,69 @@
+program Lab7f2;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Windows,
+  SysUtils;
+procedure swap(var x,y:integer);
+var t:integer;
+begin
+t:=x;
+x:=y;
+y:=t;
+end;
+const nmax=20;
+var Mat:array[1..nmax,1..nmax] of integer;
+    Str,St,StrMat,StMat,k:integer;
+    TempMat:integer;
+begin
+ repeat
+ write('Количество строк Str= ');
+ readln(Str);
+ write('Количество столбцов St= ');
+ readln(St);
+ until (Str in [1..nmax])and(St in [1..nmax]);
+ writeln('Исходная матрица: ');
+for StrMat:=1 to Str do
+ begin
+   for StMat:=1 to St do
+    begin
+     writeln('Mat[',StrMat,' , ',Stmat,']= ');
+     readln(Mat[StrMat,StMat]);
+    end;
+   writeln;
+ end;
+writeln;
+writeln('Исходная матрица:');
+ for StrMat:=1 to Str do
+   begin
+    for StMat:=1 to St do
+    write(Mat[StrMat,StMat]:4,' ');
+    writeln;
+   end;
+ writeln;
+ for StrMat:=1 to Str do
+  for StMat:=1 to St-1 do
+   for k:=StMat+1 to St do
+    if Mat[StrMat,StMat]>Mat[StrMat,k] then
+     begin
+      TempMat:=Mat[StrMat,StMat];
+      Mat[StrMat,StMat]:=Mat[StrMat,k];
+      Mat[StrMat,k]:=TempMat;
+     end;
+for k:=1 to Str do
+    for StrMat:=1 to Str-1 do
+     if Mat[StrMat,St]<Mat[StrMat+1,St] then
+      begin
+      for StMat:=1 to St do
+        swap(Mat[StrMat,StMat],Mat[StrMat+1,StMat]);
+      end;
+ writeln('Отсортированный массив: ');
+ for StrMat:=1 to Str do
+  begin
+   for StMat:=1 to St do
+   write(Mat[StrMat,StMat]:4);
+   writeln;
+  end;
+ readln
+end.
---
+++ Labs/1 kurs/OAIP/1 semestr/Lab7/Lab7f3.dpr
@@ -0,0 +1,78 @@
+program Lab7f3;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Windows,
+  SysUtils;
+procedure swap(var x,y:real);
+var t:real;
+begin
+t:=x;
+x:=y;
+y:=t;
+end;
+const nmax=20;
+var Mat:array[1..nmax,1..nmax] of real;
+    Str,St,StrMat,StMat,k:integer;
+    TempMat:real;
+begin
+ repeat
+ write('Количество строк Str= ');
+ readln(Str);
+ write('Количество столбцов St= ');
+ readln(St);
+ until (Str in [1..nmax])and(St in [1..nmax]);
+ writeln('Исходная матрица: ');
+for StrMat:=1 to Str do
+ begin
+   for StMat:=1 to St do
+    begin
+     writeln('Mat[',StrMat,' , ',Stmat,']= ');
+     readln(Mat[StrMat,StMat]);
+    end;
+   writeln;
+ end;
+writeln;
+writeln('Исходная матрица:');
+ for Strmat:=1 to str do
+   begin
+    for stmat:=1 to st do
+    begin
+    if Frac(mat[strmat,stmat])=0 then
+      write(Trunc(mat[strmat,stmat]),' ')
+    else
+    write(mat[strmat,stmat]:3:1,' ');
+    end;
+    writeln;
+   end;
+   writeln;
+ for StrMat:=1 to Str do
+  for StMat:=1 to St-1 do
+   for k:=StMat+1 to St do
+    if Mat[StrMat,StMat]>Mat[StrMat,k] then
+     begin
+      TempMat:=Mat[StrMat,StMat];
+      Mat[StrMat,StMat]:=Mat[StrMat,k];
+      Mat[StrMat,k]:=TempMat;
+     end;
+for k:=1 to Str do
+    for StrMat:=1 to Str-1 do
+     if Mat[StrMat,St]<Mat[StrMat+1,St] then
+      for StMat:=1 to St do
+        swap(Mat[StrMat,StMat],Mat[StrMat+1,StMat]);
+ writeln('Преобразованная матрица');
+  for Strmat:=1 to str do
+   begin
+    for stmat:=1 to st do
+    begin
+    if Frac(mat[strmat,stmat])=0 then
+      write(Trunc(mat[strmat,stmat]),' ')
+    else
+    write(mat[strmat,stmat]:3:1,' ');
+    end;
+    writeln;
+   end;
+   writeln;
+ readln
+end.
