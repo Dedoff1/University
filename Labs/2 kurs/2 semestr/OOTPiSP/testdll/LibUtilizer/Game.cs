19601a72cd64ecb075397bc5630dfbe1a6486fb8
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-19 21:19:25 +0300

    Add files via upload

---
+++ Labs/2 kurs/2 semestr/OOTPiSP/testdll/LibUtilizer/Game.cs
@@ -0,0 +1,45 @@
+﻿
+using oop.DisplayObjects;
+
+namespace oop
+{
+    public class Game
+    { 
+
+        private GameField gameField;
+        public int FPS;
+
+        public Game(GameField field, int fps) {
+            gameField = field;
+            FPS = fps;
+        }
+
+        public int? AddObject(DisplayObject obj)
+        {
+            return gameField.AddObject(obj);
+        }
+
+        public bool DeleteObject(int index)
+        {
+            return gameField.DeleteObject(index);
+
+        }
+        public bool DeleteObject(DisplayObject obj)
+        {
+            return gameField.DeleteObject(obj);
+        }
+
+        public void MoveObjects(double time) { 
+            gameField.MoveObjects(time);
+        }
+
+        public void DrawGame(Graphics g) {
+            gameField.Draw(g);
+        }
+
+        public bool HandleClick(int x, int y) {
+            return gameField.Click(x, y);
+        }
+        
+    }
+}
