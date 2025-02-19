6ea1eb00379d4a336994de7266198ae488817106
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-19 17:12:06 +0300

    Add files via upload

---
+++ Labs/3 kurs/1 semestr/RIAT/Riat/FishingCatalog/FishingCatalog.Postgres/Configuration/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿
+
+using FishingCatalog.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace FishingCatalog.Postgres.Configuration
+{
+    public class UserConfiguretion : IEntityTypeConfiguration<User>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.HasMany(u => u.Carts)
+                .WithOne(c => c.User);
+
+            builder.HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+
+        }
+    }
+}
---
+++ Labs/3 kurs/1 semestr/RIAT/Riat/FishingCatalog/FishingCatalog.Postgres/FishingCatalogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FishingCatalog.Core;
+using FishingCatalog.Postgres.Configuration;
+
+namespace FishingCatalog.Postgres
+{
+    public class FishingCatalogDbContext(DbContextOptions<FishingCatalogDbContext> options) : DbContext(options)
+    {
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguretion());
+            modelBuilder.ApplyConfiguration(new RoleConfiguretion());
+            modelBuilder.ApplyConfiguration(new ProductConfiguretion());
+            modelBuilder.ApplyConfiguration(new CartConfiguretion());
+            modelBuilder.ApplyConfiguration(new FeedbackConfigurating());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
