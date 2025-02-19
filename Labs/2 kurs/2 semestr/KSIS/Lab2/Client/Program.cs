37f8252be4c962d58f480e9d5cfd91c94a578139
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-19 20:28:20 +0300

    Add files via upload

---
+++ Labs/2 kurs/2 semestr/KSIS/Lab2/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpClient
+{
+    private const int port = 8888;
+    private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    static void Main(string[] args)
+    {
+        using (System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient())
+        {
+            client.Connect(ipAddress, port);
+            Console.WriteLine("Подключено к серверу.");
+            NetworkStream stream = client.GetStream();
+            while (true)
+            {
+                Console.Write("Введите сообщение: ");
+                string message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message))
+                {
+                    break;
+                }
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Отправлено: " + message);
+                byte[] responseData = new byte[256];
+                int bytes = stream.Read(responseData, 0, responseData.Length);
+                string response = Encoding.Unicode.GetString(responseData, 0, bytes);
+                Console.WriteLine("Сервер: " + response);
+            }
+            Console.WriteLine("Отключено от сервера.");
+        }
+    }
+}
---
+++ Labs/2 kurs/2 semestr/KSIS/Lab2/Client2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpClient2
+{
+    private const int port = 8888;
+    private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    static void Main(string[] args)
+    {
+        TcpClient client = new TcpClient();
+        client.Connect(ipAddress, port);
+        Console.WriteLine("Подключение к серверу установлено.");
+        NetworkStream stream = client.GetStream();
+        while (true)
+        {
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            byte[] responseData = new byte[256];
+            int bytes = stream.Read(responseData, 0, responseData.Length);
+            string response = Encoding.Unicode.GetString(responseData, 0, bytes);
+            Console.WriteLine("Ответ от сервера: " + response);
+            if (string.IsNullOrEmpty(message))
+            {
+                break;
+            }
+        }
+        client.Close();
+    }
+}
+
