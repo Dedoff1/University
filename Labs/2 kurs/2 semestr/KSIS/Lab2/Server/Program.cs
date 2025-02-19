53dd7d61272e5dc2b6406669e54012ecb2f91a46
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-19 20:30:33 +0300

    Add files via upload

---
+++ Labs/2 kurs/2 semestr/KSIS/Lab2/Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class TcpServer
+{
+    private const int port = 8888;
+    static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+        server.Start();
+        Console.WriteLine("Сервер запущен. Ожидание подключений...");
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Новый клиент подключен.");
+            Thread clientThread = new Thread(() => HandleClient(client));
+            clientThread.Start();
+        }
+    }
+
+    static void HandleClient(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        while (true)
+        {
+            byte[] data = new byte[256];
+            int bytes = stream.Read(data, 0, data.Length);
+            string message = Encoding.Unicode.GetString(data, 0, bytes);
+            Console.WriteLine("Получено сообщение: " + message);
+            if (string.IsNullOrEmpty(message))
+            {
+                client.Close();
+                break;
+            }
+            foreach (TcpClient otherClient in clients)
+            {
+                if (otherClient != client)
+                {
+                    NetworkStream otherStream = otherClient.GetStream();
+                    otherStream.Write(data, 0, bytes);
+                }
+            }
+        }
+    }
+}
