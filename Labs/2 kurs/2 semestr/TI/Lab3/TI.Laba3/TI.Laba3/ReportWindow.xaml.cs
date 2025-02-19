aa2a8352323be23d80021c583161aa17785c58e1
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-19 16:21:14 +0300

    Add files via upload

---
+++ Labs/2 kurs/2 semestr/TI/Lab3/TI.Laba3/TI.Laba3/ReportWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace TheoryOfInformation.lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для ReportWindow.xaml
+    /// </summary>
+    public partial class ReportWindow : Window
+    {
+        public ReportWindow()
+        {
+            InitializeComponent();
+            this.Background = Brushes.Yellow;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
