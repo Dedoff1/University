6910b9625f3b9503e50ca6340ae005e9719214f1
Dedoff1 <128306784+Dedoff1@users.noreply.github.com>
2025-02-19 20:21:39 +0300

    Add files via upload

---
+++ Labs/2 kurs/2 semestr/KSIS/Lab_2/Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+class Server
+{
+    static List<NetworkStream> connectedClients = new List<NetworkStream>();
+
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 8888);
+        server.Start();
+        Console.WriteLine("Server has started.");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+            connectedClients.Add(stream);
+            Console.WriteLine("Client connected.");
+
+            Thread thread = new Thread(() => HandleClient(stream));
+            thread.Start();
+        }
+    }
+
+    static void HandleClient(NetworkStream stream)
+    {
+        byte[] buffer = new byte[1024];
+        while (true)
+        {
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead == 0)
+            {
+                connectedClients.Remove(stream);
+                break;
+            }
+            string currentTime = DateTime.Now.ToString("HH:mm");
+            string message = $"{currentTime}: {Encoding.ASCII.GetString(buffer, 0, bytesRead)}";
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+
+            foreach (var clientStream in connectedClients)
+            {
+                clientStream.Write(messageBytes, 0, messageBytes.Length);
+            }
+        }
+    }
+}
